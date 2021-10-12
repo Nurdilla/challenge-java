ddc256905b8cc5871c0d31c58cfddcfdd3374dd5
Nurdilla <nurdilladilla209@gmail.com>
2021-10-12 13:00:28 +0700

    inisial comit

---
+++ DillaJava.java
@@ -0,0 +1,20 @@
+public class DillaJava{
+ public static void main(String[] args){
+    String firstName = "Nur";
+    String lastName = "dilla";
+    int age = 20;
+    String address = "Batubara";
+    String phoneNumber = "099199261";
+    String hobby = "reading";
+    String favoriteFood = "bakso";
+    System.out.println("firstName :" +firstName );
+    System.out.println("lastName :" +lastName);
+    System.out.println("age :" +age);
+    System.out.println("address :" +address);
+    System.out.println("phoneNumber :" +phoneNumber);
+    System.out.println("hobby :" +hobby);
+    System.out.println("favoriteFood :" +favoriteFood);
+
+
+ }
+}
