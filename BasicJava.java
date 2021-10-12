cfea4a0d25909892050c431c68583a8b961450d9
Nurdilla <nurdilladilla209@gmail.com>
2021-10-12 14:32:33 +0700

    Initial commit

---
+++ BasicJava.java
@@ -0,0 +1,7 @@
+public class BasicJava{
+    public static void main(String[] args) {
+    String firstName = "12341234" ; // 1
+    int number = 1234;
+    System.out.println(firstName + number);  
+   }
+}
